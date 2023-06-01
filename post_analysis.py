08d9728459d77b3c9f8fae5cff39b70ddb193c41
zsjstart <104205668+zsjstart@users.noreply.github.com>
2023-06-01 12:36:05 +0200

    Create post_analysis.py

---
+++ post_analysis.py
@@ -0,0 +1,28 @@
+def main():
+    # post_analyzer()
+    # total_statistics()
+    # historical_data_analysis()
+    # collect_rpki_invalid_routes()
+    # benign_conflicts_statistics()
+
+    # quarantined_asbindings_post_analysis()
+    
+    # plot_actant()
+    # plot_distance()
+    
+    # plot_jck()
+    # plot_benign_conflicts_numbers()
+
+    # plot_occurrence_frequencies()
+
+    # plot_events()
+    # hijack_events_statistics()
+    # bgp_attacks_statistics()
+   
+    # compare_with_BGPmon()
+    # bgp_measurements()
+
+
+
+if __name__ == "__main__":
+    main()
